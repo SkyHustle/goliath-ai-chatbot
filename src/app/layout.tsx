43af5a9e44a61a19289b9005ffd5d0a48b392b61
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-27 08:32:36 -0700

    Integrate example drawer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-
 import { cn } from "@/lib/utils";
 
 export const fontSans = FontSans({
@@ -21,14 +20,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <head />
             <body
                 className={cn(
                     "min-h-screen bg-background font-sans antialiased",
                     fontSans.variable
                 )}
             >
-                ...
+                {children}
             </body>
         </html>
     );

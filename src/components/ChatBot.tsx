97ec6832c43d7ce375420a8c52e707e736046560
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-27 17:22:49 -0700

    Messages boilerplate, remove unneeded Drawer components, structure and style the chat message container, add trashcan icon with clear message history feature

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from "react";
-
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
     Drawer,
@@ -12,42 +12,122 @@ import {
     DrawerTitle,
     DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Input } from "./ui/input";
+import { Trash, Bot } from "lucide-react";
+
+type Message = {
+    id: string;
+    role: "user" | "assistant";
+    content: string;
+    createdAt: string;
+};
+
+const messagesExample: Message[] = [
+    {
+        content: "Hey there",
+        role: "user",
+        createdAt: "2023-12-27T23:44:12.590Z",
+        id: "OZ2xRyO",
+    },
+    {
+        id: "a1scQIm",
+        createdAt: "2023-12-27T23:44:16.254Z",
+        content: "Hello! How can I assist you today?",
+        role: "assistant",
+    },
+    {
+        content: "I want to put out the trash",
+        role: "user",
+        createdAt: "2023-12-27T23:44:29.406Z",
+        id: "ubbOGzY",
+    },
+    {
+        id: "z7CH5Vb",
+        createdAt: "2023-12-27T23:44:30.691Z",
+        content:
+            "That's great! Putting out the trash regularly is an important task. It's good that you have a specific schedule for trash collection every Monday. Just make sure to gather all the trash bags and take them out to the designated area or bin before the collection time. If you have any other questions or need further assistance, feel free to ask!",
+        role: "assistant",
+    },
+    {
+        content: "Thanks",
+        role: "user",
+        createdAt: "2023-12-27T23:44:37.361Z",
+        id: "JDbMmkK",
+    },
+    {
+        id: "XP9EBet",
+        createdAt: "2023-12-27T23:44:38.454Z",
+        content:
+            "You're welcome! If you have any more questions or need assistance with anything else, feel free to ask.",
+        role: "assistant",
+    },
+];
 
 export function ChatBot() {
+    const [messages, setMessages] = React.useState(messagesExample);
+
     return (
         <Drawer>
             <DrawerTrigger asChild>
-                <Button variant="outline">Open Drawer</Button>
+                <Button variant="outline">Open ChatBot</Button>
             </DrawerTrigger>
             <DrawerContent>
-                <div className="mx-auto w-full max-w-sm bg-red-300">
-                    <DrawerHeader>
-                        <DrawerTitle>Title</DrawerTitle>
-                        <DrawerDescription>
-                            Description in here
-                        </DrawerDescription>
-                    </DrawerHeader>
-                    <div className="p-4 pb-0">
-                        <div className="flex items-center justify-center space-x-2">
-                            <div className="flex-1 text-center">
-                                <div className="text-7xl font-bold tracking-tighter">
-                                    Messages
-                                </div>
-                                <div className="text-[0.70rem] uppercase text-muted-foreground">
-                                    Description
-                                </div>
+                <DrawerClose asChild></DrawerClose>
+                <div className="flex h-[600px] w-[400px] flex-col">
+                    <div
+                        className="mt-3 h-full overflow-y-auto px-3"
+                        // ref={scrollRef}
+                    >
+                        {messages.map((message) => (
+                            <ChatMessage message={message} key={message.id} />
+                        ))}
+
+                        {messages.length === 0 && (
+                            <div className="flex h-full items-center justify-center gap-3">
+                                <Bot />
+                                Ask Ai a question
                             </div>
-                        </div>
-                        <div className="mt-3 h-[120px]">Data</div>
+                        )}
                     </div>
-                    <DrawerFooter>
-                        Footer
-                        <DrawerClose asChild>
-                            <Button variant="outline">Close Drawer</Button>
-                        </DrawerClose>
-                    </DrawerFooter>
+                    {/* Regular HTML form, don't need input validation, form logic handled by vercel ai SDK */}
+                    <form
+                        // onSubmit={handleSubmit}
+                        className="m-3 flex gap-1"
+                    >
+                        {/* shadcn Input and Button look better */}
+                        <Button
+                            title="Clear Chat"
+                            variant="outline"
+                            size="icon"
+                            className="shrink-0"
+                            type="button"
+                            onClick={() => setMessages([])}
+                        >
+                            <Trash />
+                        </Button>
+                        <Input placeholder="Ask away" />
+                        <Button type="submit">Send</Button>
+                    </form>
                 </div>
             </DrawerContent>
         </Drawer>
+    );
+}
+
+function ChatMessage({ message: { role, content } }: { message: Pick<Message, "role" | "content"> }) {
+    const isAiMessage = role === "assistant";
+
+    return (
+        <div className={cn("mb-3 flex items-center", isAiMessage ? "me-5 justify-start" : "ms-5 justify-end")}>
+            {isAiMessage && <Bot className="mr-2 shrink-0" />}
+            <p
+                className={cn(
+                    "whitespace-pre-line rounded-md border px-3 py-2",
+                    isAiMessage ? "bg-background" : "bg-primary text-primary-foreground"
+                )}
+            >
+                {content}
+            </p>
+        </div>
     );
 }

5c27ea7b447187180306ed0111ea8960e1d5a7b4
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-28 18:21:30 -0700

    Basic end to end OpenAI streaming chat functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { ChatBot } from "@/components/ChatBot";
 import ChatCompletion from "@/components/ChatCompletion";
+import Chat from "@/components/Chat";
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center p-32">
             <ChatBot />
-            <ChatCompletion />
+            <Chat />
         </main>
     );
 }

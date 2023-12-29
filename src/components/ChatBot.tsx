879f5268c1cf96d8a06b653c67f800adcd4f1828
Dmitry Sky <dmitry.sky@icloud.com>
2023-12-28 19:32:28 -0700

    Wire up Chatbot Ui to vercel's ai SDK

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -14,75 +14,14 @@ import {
 } from "@/components/ui/drawer";
 import { Input } from "./ui/input";
 import { Trash, Bot } from "lucide-react";
-
-type Message = {
-    id: string;
-    role: "user" | "assistant";
-    content: string;
-    createdAt: string;
-};
-
-const messagesExample: Message[] = [
-    {
-        content: "Hey there",
-        role: "user",
-        createdAt: "2023-12-27T23:44:12.590Z",
-        id: "OZ2xRyO",
-    },
-    {
-        id: "a1scQIm",
-        createdAt: "2023-12-27T23:44:16.254Z",
-        content: "Hello! How can I assist you today?",
-        role: "assistant",
-    },
-    {
-        content: "I want to put out the trash",
-        role: "user",
-        createdAt: "2023-12-27T23:44:29.406Z",
-        id: "ubbOGzY",
-    },
-    {
-        id: "z7CH5Vb",
-        createdAt: "2023-12-27T23:44:30.691Z",
-        content:
-            "That's great! Putting out the trash regularly is an important task. It's good that you have a specific schedule for trash collection every Monday. Just make sure to gather all the trash bags and take them out to the designated area or bin before the collection time. If you have any other questions or need further assistance, feel free to ask!",
-        role: "assistant",
-    },
-    {
-        content: "Thanks",
-        role: "user",
-        createdAt: "2023-12-27T23:44:37.361Z",
-        id: "JDbMmkK",
-    },
-    {
-        id: "XP9EBet",
-        createdAt: "2023-12-27T23:44:38.454Z",
-        content:
-            "You're welcome! If you have any more questions or need assistance with anything else, feel free to ask.",
-        role: "assistant",
-    },
-];
+import { useChat } from "ai/react";
+import { Message } from "ai";
 
 export function ChatBot() {
-    const [messages, setMessages] = useState(messagesExample);
-    const [input, setInput] = useState("");
+    const { messages, error, isLoading, input, setMessages, handleInputChange, handleSubmit } = useChat();
 
     // ref allows us to access a HTML Element Directly
     const inputRef = useRef<HTMLInputElement>(null);
-
-    function handleClearChat() {
-        setMessages([]);
-        // After clearing the chat, focus the input field
-        inputRef.current?.focus();
-    }
-
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        setMessages([...messages, { id: "1", role: "user", content: input, createdAt: "2023-12-27T23:44:12.590Z" }]);
-        setInput("");
-        // After submitting the form, focus the input field
-        inputRef.current?.focus();
-    }
 
     return (
         <Drawer>
@@ -116,16 +55,16 @@ export function ChatBot() {
                             size="icon"
                             className="shrink-0"
                             type="button"
-                            onClick={handleClearChat}
+                            onClick={() => setMessages([])}
                         >
                             <Trash />
                         </Button>
                         <Input
                             autoFocus={true}
                             value={input}
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setInput(event.target.value)}
-                            placeholder="Ask away"
-                            ref={inputRef}
+                            onChange={handleInputChange}
+                            placeholder="Ask a question..."
+                            // ref={inputRef}
                         />
                         <Button type="submit">Send</Button>
                     </form>
